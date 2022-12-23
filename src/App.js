26dcc355ac1ba80f5a30e35450e9406af52534f9
Suresh Maurya <sureshmaurya48@gmail.com>
2022-12-23 08:48:44 +0530

    added

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
 <div className="App">
   <Header/>
    <Routes>
-    <Route path='/' element={<Home/>}/>
+    <Route path='/ecommerce' element={<Home/>}/>
     {/* <Route path='/cart' element={<Cart/>}/> */}
     <Route path="/product-details" element={<Details/>}/>
    </Routes>
